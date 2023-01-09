51c58a6507c795f56611a3a66ef0f0d1cf24e659
Kislaya Srivastava <akshat.sri2605@gmail.com>
2023-01-09 22:09:15 +0530

    Jenkinsfiles changed

--- Api1/src/main/java/com/example/Api1/Api1Application.java
+++ Api1/src/main/java/com/example/Api1/Api1Application.java
@@ -2,6 +2,8 @@ package com.example.Api1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Api1Application extends SpringBootServletInitializer {
