ac6011c1b4e2df553b8dd70d8bdf70f1d4c1425c
Kislaya Srivastava <akshat.sri2605@gmail.com>
2023-01-10 13:22:39 +0530

    App code changed

--- Api1/src/main/java/com/example/Api1/Api1Application.java
+++ Api1/src/main/java/com/example/Api1/Api1Application.java
@@ -2,16 +2,9 @@ package com.example.Api1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Api1Application extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	   return application.sources(Api1Application.class);
-	}
+public class Api1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Api1Application.class, args);
